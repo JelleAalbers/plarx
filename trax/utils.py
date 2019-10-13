d4a766216bec6820bba71305efcfa08d8ceefc98
Jelle Aalbers <jelleaalbers@gmail.com>
2019-10-13 18:31:54 +0200

    Initial draft

---
+++ trax/utils.py
@@ -0,0 +1,15 @@
+
+
+def exporter(export_self=False):
+    """Export utility modified from https://stackoverflow.com/a/41895194
+    Returns export decorator, __all__ list
+    """
+    all_ = []
+    if export_self:
+        all_.append('exporter')
+
+    def decorator(obj):
+        all_.append(obj.__name__)
+        return obj
+
+    return decorator, all_
